83e76afe87a00a4a3e2f703a4dceb999a8bd29c6
Llerak <llerak.exe@gmail.com>
2023-02-21 02:33:23 -0500

    fix

--- src/components/modules/ButtonTaskList.tsx
+++ src/components/modules/ButtonTaskList.tsx
@@ -12,15 +12,3 @@ const ButtonTaskList = ({ children, className}: any) => {
 
 
 export default ButtonTaskList;
-
-export const PrimaryButton = ({ children, className, ...props }: any) => {
-    const style =
-      " border mt-2 py-1 px-3 bg-primary rounded border-[#7691ad] text-footer-text hover:bg-white hover:text-primary ";
-    return (
-      <>
-        <button {...props} className={style + " " + className}>
-          {children}
-        </button>
-      </>
-    );
-  };
