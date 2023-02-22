e3f40b3ab940fd5b0d020b3545fd2cb176da277f
Llerak <llerak.exe@gmail.com>
2023-02-22 12:08:32 -0500

    add background color to buttons

--- src/components/modules/ButtonTaskList.tsx
+++ src/components/modules/ButtonTaskList.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ButtonTaskList = ({ children, className}: any) => {
   const style: string =
-    "cursor-pointer flex items-center py-0.5 px-4 border rounded-md m-0.5 h-8 text-sm font-mono";
+    "cursor-pointer flex items-center py-0.5 px-4 border rounded-md m-0.5 h-8 text-sm font-mono bg-[#EAF0F5]";
   return (
     <div className={style + " " + className}>
       {children}
