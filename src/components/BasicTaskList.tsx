d2e1310acc4ec8790c81a654e83fb4b02cd76c7b
EmmeKoZzz <emme.ko.zzz@gmail.com>
2023-02-22 12:05:17 -0500

    fixing style in plus button

--- src/components/BasicTaskList.tsx
+++ src/components/BasicTaskList.tsx
@@ -1,55 +1,59 @@
-import React from "react";
 import { useState } from "react";
 import {
-  plusSquareIcon,
-  calendarIcon,
-  discIcon,
-  loaderIcon,
-  maximizeIcon,
-  unlockIcon,
+	calendarIcon,
+	discIcon,
+	loaderIcon,
+	maximizeIcon,
+	plusSquareIcon,
+	unlockIcon,
 } from "../assets/Icons";
 import ButtonTaskList from "./modules/ButtonTaskList";
 
 const BasicTaskList = () => {
-  const [showDropDown, setShowDropDown] = useState(false);
+	const [showDropDown, setShowDropDown] = useState(false);
 
-  return (
-    <div className="flex flex-col rounded-md max-w-4xl">
-      <a
-        href="#"
-        className="flex pb-4"
-        onClick={() => setShowDropDown(!showDropDown)}
-      >
-        {plusSquareIcon}
-        <input
-          type="text"
-          placeholder="Type to add new task"
-          className="cursor-pointer font-serif  outline-none"
-        />
-      </a>
+	const borderStyle = "border rounded-md";
 
-      {showDropDown && (
-        <div
-          className="flex border-
+	return (
+		<div
+			className={
+				"flex flex-col content-center " + (showDropDown && borderStyle)
+			}
+		>
+			<div
+				className="flex py-2"
+				onChange={() => setShowDropDown(!showDropDown)}
+			>
+				{plusSquareIcon}
+				<input
+					type="text"
+					placeholder="Type to add new task"
+					className="cursor-pointer font-serif  outline-none"
+				/>
+			</div>
+
+			{showDropDown && (
+				<div
+					className="flex border-
          justify-between border-t"
-        >
-          <div className="flex p-1">
-            <ButtonTaskList href="#" className="mr-8 disabled">
-              {maximizeIcon}Open
-            </ButtonTaskList>
-            <ButtonTaskList href="#">{calendarIcon}Today</ButtonTaskList>
-            <ButtonTaskList href="#">{unlockIcon}Public</ButtonTaskList>
-            <ButtonTaskList href="#">{discIcon}Normal</ButtonTaskList>
-            <ButtonTaskList href="#">{loaderIcon}Estimation</ButtonTaskList>
-          </div>
-          <div className="flex items-center">
-            <ButtonTaskList href="#">Cancel</ButtonTaskList>
-            <ButtonTaskList href="#">Ok</ButtonTaskList>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+				>
+					<div className="flex p-1">
+						<ButtonTaskList href="#" className="mr-8 disabled">
+							{maximizeIcon}Open
+						</ButtonTaskList>
+						<ButtonTaskList href="#">{calendarIcon}Today</ButtonTaskList>
+						<ButtonTaskList href="#">{unlockIcon}Public</ButtonTaskList>
+						<ButtonTaskList href="#">{discIcon}Normal</ButtonTaskList>
+						<ButtonTaskList href="#">{loaderIcon}Estimation</ButtonTaskList>
+					</div>
+					<div className="flex items-center">
+						<ButtonTaskList href="#">Cancel</ButtonTaskList>
+						<ButtonTaskList href="#">Ok</ButtonTaskList>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default BasicTaskList;
