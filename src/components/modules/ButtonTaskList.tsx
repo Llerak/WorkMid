1f439a7179f9b9bfa84541af3058ede6093e75b0
Llerak <llerak.exe@gmail.com>
2023-02-22 11:51:25 -0500

    change a to div

--- src/components/modules/ButtonTaskList.tsx
+++ src/components/modules/ButtonTaskList.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const ButtonTaskList = ({ children, className, href}: any) => {
+const ButtonTaskList = ({ children, className}: any) => {
   const style: string =
-    "flex items-center py-0.5 px-4 border rounded-md m-0.5 h-8 text-sm font-mono";
+    "cursor-pointer flex items-center py-0.5 px-4 border rounded-md m-0.5 h-8 text-sm font-mono";
   return (
-    <a href={href} className={style + " " + className}>
+    <div className={style + " " + className}>
       {children}
-    </a>
+    </div>
   );
 };
 
