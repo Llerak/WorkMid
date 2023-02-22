62e0c3a476c74111e3c10fb6e5ffc26402c75593
EmmeKoZzz <emme.ko.zzz@gmail.com>
2023-02-22 12:49:27 -0500

    handling insert new task text

--- src/components/BasicTaskList.tsx
+++ src/components/BasicTaskList.tsx
@@ -9,55 +9,61 @@ import {
 } from "../assets/Icons";
 import ButtonTaskList from "./modules/ButtonTaskList";
 
-const initialState = {
-	menuDisplay: false,
-	textInput: "",
-};
 const BasicTaskList = () => {
-	const [showDropDown, setShowDropDown] = useState(initialState);
+	const [menuDisplay, setMenuDisplay] = useState(false);
+	const [inputText, setInputText] = useState("");
 
 	const borderStyle = "border rounded-md";
 
+	const handleNewTaskInput = (e: any) => {
+		setInputText(e.target.value);
+		console.log(inputText);
+	};
+
 	return (
 		<div
-			className={
-				"flex flex-col content-center " + (showDropDown && borderStyle)
-			}
+			className={"flex flex-col content-center " + (menuDisplay && borderStyle)}
 		>
-			<div
-				className="flex py-2"
-				onFocus={() => setShowDropDown({ menuDisplay: true, ...textInput })}
-			>
+			<div className="flex py-2">
 				{plusSquareIcon}
 				<input
 					type="text"
+					onChange={handleNewTaskInput}
 					placeholder="Type to add new task"
 					className="cursor-pointer font-serif  outline-none"
 				/>
 			</div>
 
-			{showDropDown && (
+			{menuDisplay && (
 				<div
 					className="flex border-
          justify-between border-t"
-        >
-          <div className="flex p-1">
-            <ButtonTaskList  className="mr-8 disabled">
-              {maximizeIcon}Open
-            </ButtonTaskList>
-            <ButtonTaskList className="disabled-button">{calendarIcon}Today</ButtonTaskList>
-            <ButtonTaskList className="disabled-button">{unlockIcon}Public</ButtonTaskList>
-            <ButtonTaskList className="disabled-button">{discIcon}Normal</ButtonTaskList>
-            <ButtonTaskList className="disabled-button">{loaderIcon}Estimation</ButtonTaskList>
-          </div>
-          <div className="flex items-center">
-            <ButtonTaskList >Cancel</ButtonTaskList>
-            <ButtonTaskList >Ok</ButtonTaskList>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+				>
+					<div className="flex p-1">
+						<ButtonTaskList className="mr-8 disabled">
+							{maximizeIcon}Open
+						</ButtonTaskList>
+						<ButtonTaskList className="disabled-button">
+							{calendarIcon}Today
+						</ButtonTaskList>
+						<ButtonTaskList className="disabled-button">
+							{unlockIcon}Public
+						</ButtonTaskList>
+						<ButtonTaskList className="disabled-button">
+							{discIcon}Normal
+						</ButtonTaskList>
+						<ButtonTaskList className="disabled-button">
+							{loaderIcon}Estimation
+						</ButtonTaskList>
+					</div>
+					<div className="flex items-center">
+						<ButtonTaskList>Cancel</ButtonTaskList>
+						<ButtonTaskList>Ok</ButtonTaskList>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default BasicTaskList;
