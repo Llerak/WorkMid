660592dc62d8f5dd30c41ad265c372e427e1b2cd
Llerak <llerak.exe@gmail.com>
2023-02-22 13:42:26 -0500

    add useEffect to element by id button-ok

--- src/components/BasicTaskList.tsx
+++ src/components/BasicTaskList.tsx
@@ -1,68 +1,75 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
-	calendarIcon,
-	discIcon,
-	loaderIcon,
-	maximizeIcon,
-	plusSquareIcon,
-	unlockIcon,
+  calendarIcon,
+  discIcon,
+  loaderIcon,
+  maximizeIcon,
+  plusSquareIcon,
+  unlockIcon,
 } from "../assets/Icons";
 import ButtonTaskList from "./modules/ButtonTaskList";
 
 const BasicTaskList = () => {
-	const [menuDisplay, setMenuDisplay] = useState(false);
-	const [inputText, setInputText] = useState("");
+  const [menuDisplay, setMenuDisplay] = useState(false);
+  const [inputText, setInputText] = useState("");
 
-	const borderStyle = "border rounded-md";
+  const borderStyle = "border rounded-md";
 
-	const handleWriting = (e: any) => {
-		const { value } = e.target;
-		setInputText(value);
-		value !== "" ? setMenuDisplay(true) : setMenuDisplay(false);
-	};
+  const handleWriting = (e: any) => {
+    const { value } = e.target;
+    setInputText(value);
+    value !== "" ? setMenuDisplay(true) : setMenuDisplay(false);
+  };
 
-	return (
-		<div
-			className={"flex flex-col content-center " + (menuDisplay && borderStyle)}
-		>
-			<div className="flex py-2">
-				{plusSquareIcon}
-				<input
-					type="text"
-					onChange={handleWriting}
-					onFocus={() => setMenuDisplay(true)}
-					placeholder="Type to add new task"
-					className="cursor-pointer font-serif  outline-none"
-				/>
-			</div>
+  useEffect(() => {
+    if (inputText !== "") {
+      const buttonOk = document.getElementById("button-ok");
+      if (buttonOk != null) buttonOk.innerHTML = "Add";
+    }
+  }, [inputText]);
 
-			{menuDisplay && (
-				<div className="flex justify-between border-t p-1">
-					<div className="flex">
-						<ButtonTaskList className="mr-8">{maximizeIcon}Open</ButtonTaskList>
-						<ButtonTaskList className="disabled-button">
-							{calendarIcon}Today
-						</ButtonTaskList>
-						<ButtonTaskList className="disabled-button">
-							{unlockIcon}Public
-						</ButtonTaskList>
-						<ButtonTaskList className="disabled-button">
-							{discIcon}Normal
-						</ButtonTaskList>
-						<ButtonTaskList className="disabled-button">
-							{loaderIcon}Estimation
-						</ButtonTaskList>
-					</div>
-					<div className="flex">
-						<ButtonTaskList>Cancel</ButtonTaskList>
-						<ButtonTaskList className="bg-[#0D55CF] text-white">
-							Ok
-						</ButtonTaskList>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div
+      className={"flex flex-col content-center " + (menuDisplay && borderStyle)}
+    >
+      <div className="flex py-2">
+        {plusSquareIcon}
+        <input
+          type="text"
+          onChange={handleWriting}
+          onFocus={() => setMenuDisplay(true)}
+          placeholder="Type to add new task"
+          className="cursor-pointer font-serif  outline-none"
+        />
+      </div>
+
+      {menuDisplay && (
+        <div className="flex justify-between border-t p-1">
+          <div className="flex">
+            <ButtonTaskList className="mr-8">{maximizeIcon}Open</ButtonTaskList>
+            <ButtonTaskList className="disabled-button">
+              {calendarIcon}Today
+            </ButtonTaskList>
+            <ButtonTaskList className="disabled-button">
+              {unlockIcon}Public
+            </ButtonTaskList>
+            <ButtonTaskList className="disabled-button">
+              {discIcon}Normal
+            </ButtonTaskList>
+            <ButtonTaskList className="disabled-button">
+              {loaderIcon}Estimation
+            </ButtonTaskList>
+          </div>
+          <div className="flex">
+            <ButtonTaskList>Cancel</ButtonTaskList>
+            <ButtonTaskList className="bg-[#0D55CF] text-white" id="button-ok">
+              Ok
+            </ButtonTaskList>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default BasicTaskList;
