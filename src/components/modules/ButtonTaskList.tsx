26035fc6996e2c32c2e710fd3927e8e2947bec82
Llerak <llerak.exe@gmail.com>
2023-02-23 22:40:35 -0500

    add onClick to the buttons

--- src/components/modules/ButtonTaskList.tsx
+++ src/components/modules/ButtonTaskList.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ButtonTaskList = ({ children, className, id}: any) => {
+const ButtonTaskList = ({ children, className, id, onClick}: any) => {
   const style: string =
     "cursor-pointer flex items-center py-0.5 px-4 border rounded-md m-0.5 h-8 text-sm font-mono bg-[#EAF0F5]";
   return (
-    <div className={style + " " + className} id={id}>
+    <div className={style + " " + className} id={id} onClick={onClick}>
       {children}
     </div>
   );
