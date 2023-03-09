3969835e7c8160a67f0c845d5450e66ec2afdb6e
EmmeKoZzz <emme.ko.zzz@gmail.com>
2023-03-09 06:25:49 -0500

    setting react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 import { Login } from "./components/Login/Login";
 import BasicTaskList from "./components/TaskMenu/TaskList";
@@ -14,6 +15,7 @@ export default function App() {
 			<div className="App">
 				{auth ? <BasicTaskList /> : <Login auth={setauth} />}
 			</div>
+			<ReactQueryDevtools/>
 		</QueryClientProvider>
 	);
 }
