af62fff68d1b25e5ff00d6dc8b0fe7f2ce9d9d25
Llerak <llerak.exe@gmail.com>
2023-03-10 00:01:12 -0500

    Login link with the api

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,47 +1,59 @@
 import { Dispatch, SetStateAction, SyntheticEvent, useState } from "react";
 import { handleInputWriting } from "../_utils";
 import Form from "./Form";
+import axios from "axios";
 
 type LoginProps = {
-	auth: Dispatch<SetStateAction<boolean>>;
+  auth: Dispatch<SetStateAction<boolean>>;
 };
 
 export function Login({ auth }: LoginProps) {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	const inputs = [
-		{
-			label: "Email",
-			set: (e: SyntheticEvent) => handleInputWriting(e, setEmail),
-		},
-		{
-			label: "Password",
-			set: (e: SyntheticEvent) => handleInputWriting(e, setPassword),
-		},
-	];
+  const inputs = [
+    {
+      label: "Email",
+      set: (e: SyntheticEvent) => handleInputWriting(e, setEmail),
+    },
+    {
+      label: "Password",
+      set: (e: SyntheticEvent) => handleInputWriting(e, setPassword),
+    },
+  ];
 
-	const handleSubmit = () => {};
+  const handleSubmit = () => {
+    //#Pending review and changes
+	axios
+      .post("https://luisvidal87.pythonanywhere.com/api-token-auth/", {
+        username: email,
+        password: password,
+      })
+      .then(() => {
+        auth(true);
+      })
+      .catch(() => {});
+  };
 
-	return <Form name="Sign in" input={inputs} submitAction={handleSubmit} />;
+  return <Form name="Sign in" input={inputs} submitAction={handleSubmit} />;
 }
 
 export function Signup() {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	const inputs = [
-		{
-			label: "Email",
-			set: (e: SyntheticEvent) => handleInputWriting(e, setEmail),
-		},
-		{
-			label: "Password",
-			set: (e: SyntheticEvent) => handleInputWriting(e, setPassword),
-		},
-	];
+  const inputs = [
+    {
+      label: "Email",
+      set: (e: SyntheticEvent) => handleInputWriting(e, setEmail),
+    },
+    {
+      label: "Password",
+      set: (e: SyntheticEvent) => handleInputWriting(e, setPassword),
+    },
+  ];
 
-	const handleSubmit = () => {};
+  const handleSubmit = () => {};
 
-	return <Form name="Sign un" input={inputs} submitAction={handleSubmit} />;
+  return <Form name="Sign un" input={inputs} submitAction={handleSubmit} />;
 }
